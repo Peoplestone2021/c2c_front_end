dca9f67c6d1260acf13d28cbc1248ce53b52eb3a
YANG MIN SEOK <peoplestone2021@gmail.com>
2021-11-02 12:42:35 +0900

    211102

--- pages/lobby/lobbiy.tsx
+++ pages/lobby/lobbiy.tsx
@@ -1,3 +1,43 @@
-const lobbiy = () => {}
+const lobby = () => {
+  return (
+    <div className="mx-auto">
+      <header className="header d-flex">
+        <div className="mainpage">
+          <div className="main-title d-flex">
+            <h1>환</h1>
+            <h2>전</h2>
+            <h1>장</h1>
+            <h4>터</h4>
+          </div>
+        </div>
+      </header>
+      {/* <nav className= "navi position-fixed">
+      <ul className=" flex-column">
+        <li className ="nav-item">환전 계산</li>
+        <li className ="nav-item">환율 조회</li>
+        <li className ="nav-item">기간별 매매율</li>
+      </ul>
+      </nav> */}
+      {/* main content */}
+      <div className="content">
+        <div className="menus">
+          <a href="#!" className="menu-link">
+            <div className="menu1">
+              <p>환전로비</p>
+            </div>
+          </a>
+          <a href="#!" className="menu-link">
+            <div className="menu2">마켓플레이스</div>
+          </a>
+          <a href="#" className="menu-link">
+            <div className="menu3">
+              <p>프라이빗거래매니저</p>
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default lobbiy;
+export default lobby;
---
+++ pages/market/detail/[id].tsx
@@ -0,0 +1,25 @@
+// import { AppDispatch, RootState } from "../../../provider";
+
+const MarketDetail = () => {
+  // const marketList = useSelector((state: RootState) => state.contact);
+  return;
+  // (
+  //   <section>
+  //     <h2>
+  //       {item.crcHave}
+  //       <b>/</b>
+  //       {item.cntHave}
+  //       <b>/</b>
+  //       {getTimeStringD(item.dDay)}
+  //     </h2>
+  //     <body>
+  //       <div>
+  //         <Profile></Profile>
+  //       </div>
+  //       <div></div>
+  //     </body>
+  //   </section>
+  // );
+};
+
+export default MarketDetail;
