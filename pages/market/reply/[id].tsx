6a25623fda10512a5357291abb16fe69af3ab6fd
YANG MIN SEOK <peoplestone2021@gmail.com>
2021-11-02 18:02:43 +0900

    trouble shooted

--- pages/market/reply/[id].tsx
+++ pages/market/reply/[id].tsx
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 const reply = () => {
   return;
 };
 
 export default reply;
-=======
-const Reply = () => {
-  return;
-};
-
-export default Reply;
->>>>>>> 53e46d1b08bb7e1848169f73ba5d30c380801d89
