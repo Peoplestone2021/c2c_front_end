b2bf09dfc9f30d9264ea220ce671c6420e8fe40d
YANG MIN SEOK <peoplestone2021@gmail.com>
2021-11-03 10:49:45 +0900

    market tsx not rendered

--- pages/market/market.tsx
+++ pages/market/market.tsx
@@ -1,10 +1,14 @@
 // import itemState from "../../provider/modules/market";
+import axios from "axios";
+import { NavItem, ToastBody } from "react-bootstrap";
+import styles from "../../styles/market.module.css";
+// import { flagusd } from "../market/data";
 
 // const getTimeStringD = (unixtime: number) => {
 //   return;
 // };
 
-interface MarketItem {
+interface marketItems {
   id: number;
   hostName: String;
   crcHave: String;
@@ -16,7 +20,18 @@ interface MarketItem {
   status: boolean;
 }
 
-const MarketItem = [
+interface marketItemListResponse {
+  id: number;
+  crcHave: String;
+  // crcWant: String; // 한국원 케이스 알고리즘 필요
+  cntHave: number;
+  cntWant: number;
+  dDate: String;
+  // content?: String;
+  status: boolean;
+}
+
+const marketItems = [
   {
     id: 11111111,
     hostName: "일번",
@@ -63,35 +78,103 @@ const MarketItem = [
   },
 ];
 
-// const marketItems= data.map((item)=>)
+// const MarketItemData = (marketItems) => {};
+
+// const flag = flagusd;
+
+// const marketItems= map((item)=>({
+//   id.
+// })
+// )
 
 const market = () => {
   return (
     <section>
-      <h2>마켓플레이스</h2>
-      <div></div>
-      <table>
-        <thead>
-          <tr>
-            <th scope="col">국가</th>
-            <th>금액</th>
-            <th>가격</th>
-            {/*정렬 기능 버튼처리 필요*/}
-            <th>상태</th>
-            <th>거래마감</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>{}</td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-        </tbody>
-      </table>
-      {/* 더보기 버튼, 페이징 처리 필요 */}
+      <div id="site-container" style={{ width: "50vw" }} className="mx-auto">
+        <h1 className="text-center my-5 fw-bold border-bottom pb-4">
+          마켓플레이스
+        </h1>
+        <div
+          id="search-form"
+          className="gap-2 button-group  mx-auto d-md-flex"
+          role="group"
+          style={{ width: 150 }}
+        >
+          <button className="btn btn-secondary">[검색</button>
+          <button className="btn btn-secondary">도구]</button>
+        </div>{" "}
+        {/*검색 도구*/}
+        <span className="bi bi-list badge bg-light fw-bold"> LIST</span>
+        <table className="text-center striped bordered hover w-100">
+          <thead>
+            <tr className="text-secondary">
+              <th scope="col">국가</th>
+              <th>금액</th>
+              <th>가격</th>
+              {/*정렬 기능 버튼처리 필요-후순위*/}
+              <th>상태</th>
+              <th>거래마감</th>
+            </tr>
+          </thead>
+          <tbody className="border-bottom border-top">
+            {" "}
+            {/*map으로 출력하도록*/}
+            <tr className="h-40">
+              <td>USD</td>
+              {/*state사용*/}
+              <td>1,000</td>
+              <td>1,174,000w</td>
+              <td className="bi bi-play-fill"></td>
+              <td className="text-warning">오늘</td>
+            </tr>
+            <tr>
+              <td>USD</td>
+              <td>1,100</td>
+              <td>1,291,000w</td>
+              <td className="bi bi-play-fill"></td>
+              <td>마감없음</td>
+            </tr>
+            <tr>
+              <td>USD</td>
+              <td>890</td>
+              <td>1,044,000w</td>
+              <td className="bi bi-stop-fill"></td>
+              <td>-14일</td>
+            </tr>
+            <tr>
+              <td>USD</td>
+              <td>880</td>
+              <td>1,033,000w</td>
+              <td className="bi bi-play-fill"></td>
+              <td>-7일</td>
+            </tr>
+            <tr>
+              <td>USD</td>
+              <td>1,150</td>
+              <td>1,291,000w</td>
+              <td className="bi bi-play-fill"></td>
+              <td>마감없음</td>
+            </tr>
+          </tbody>
+        </table>
+        <table className="text-center striped bordered hover w-100">
+          <thead>
+            <tr className="text-secondary">
+              <th scope="col">국가</th>
+              <th>금액</th>
+              <th>가격</th>
+              {/*정렬 기능 버튼처리 필요-후순위*/}
+              <th>상태</th>
+              <th>거래마감</th>
+            </tr>
+          </thead>
+          <tbody className="border-bottom border-top"></tbody>
+        </table>
+        <div className="text-center btn text-secondary fs-10 fw-bold mx-auto">
+          더 보기
+        </div>
+        {/* 더보기 버튼, 페이징 처리 필요 */}
+      </div>
     </section>
   );
   // 머지 테스트
