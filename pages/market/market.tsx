f85b5eb79ad87bf51c8361f6b551036157df9d32
YANG MIN SEOK <peoplestone2021@gmail.com>
2021-11-04 18:01:04 +0900

    market error fixed

--- pages/market/market.tsx
+++ pages/market/market.tsx
@@ -1,13 +1,13 @@
-// import itemState from "../../provider/modules/market";
-import axios from "axios";
-import { NavItem, ToastBody } from "react-bootstrap";
-import styles from "../../styles/market.module.css";
-import Link from "next/link";
 import { useRouter } from "next/router";
+import Image from "next/image";
+import SSRProvider from "react-bootstrap/SSRProvider";
 import { useState } from "react";
 import { GetServerSideProps } from "next";
-import Image from "next/image";
-import { userInfo } from "os";
+// import itemState from "../../provider/modules/market";
+// import axios from "axios";
+// import { NavItem, ToastBody } from "react-bootstrap";
+// import styles from "../../styles/market.module.css";
+// import Link from "next/link";
 // import {profilePic} from '../data/flag_usd.png'
 // import {}
 
@@ -41,15 +41,58 @@ import { userInfo } from "os";
 
 // }
 
-function crcHaveImg() {
-  return (
-    <>
-      <Image src="/data/flag_usd.png" width={10} height={10}></Image>
-    </>
-  );
-}
-
 const Market = () => {
+  const marketItems = [
+    {
+      id: 0,
+      hostName: "일번",
+      crcHave: "USD",
+      crcWant: "KRW",
+      cntHave: 100,
+      cntWant: 117400,
+      dDate: "1635747679",
+      content: "울릉도 동남쪽",
+      status: true,
+    },
+    {
+      id: 22222222,
+      hostName: "이번",
+      crcHave: "USD",
+      crcWant: "KRW",
+      cntHave: 110,
+      cntWant: 127400,
+      dDate: "1635745679",
+      content: "뱃길따라 이백리",
+      status: true,
+    },
+    {
+      id: 33333333,
+      hostName: "삼번",
+      crcHave: "USD",
+      crcWant: "KRW",
+      cntHave: 120,
+      cntWant: 128400,
+      dDate: "1635749679",
+      content: "외로운 섬하나",
+      status: true,
+    },
+    {
+      id: 44444444,
+      hostName: "사번",
+      crcHave: "USD",
+      crcWant: "KRW",
+      cntHave: 120,
+      cntWant: 128400,
+      dDate: "1635748679",
+      content: "새들의 고향",
+      status: true,
+    },
+  ];
+
+  // const listMarketItems = marketItems.map((marketItems) => (
+  //   <li>{listMarketItems}</li>
+  // ));
+
   const router = useRouter();
 
   // const [itemList, setItemList] = useState<MarketItems[]>
