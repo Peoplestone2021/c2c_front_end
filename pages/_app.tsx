9edca563c785cd9290f47d467391ad5100ab866b
YANG MIN SEOK <peoplestone2021@gmail.com>
2021-11-18 17:59:27 +0900

    _app.index chaged

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,5 @@
-
-
 import { Provider } from 'react-redux'
 import { store } from '../provider';
-
 
 import React from 'react'
 
@@ -12,20 +9,15 @@ import "../styles/calculator.scss";
 import "../styles/lobby.scss";
 import type { AppProps } from "next/app";
 
-
 import '@fullcalendar/common/main.css'
 import '@fullcalendar/daygrid/main.css'
 import '@fullcalendar/timegrid/main.css'
 
-
-
 function MyApp({ Component, pageProps }: AppProps) {
   return (  
     <Provider store={store}>
-
-    <Component {...pageProps} />
-   </Provider>
+     <Component {...pageProps} />
+    </Provider>
   );}
-
 
 export default MyApp;
